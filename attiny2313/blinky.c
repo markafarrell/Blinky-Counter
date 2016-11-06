29680d7a7b7156fa85bb2cfee1fabb5491880de7
markafarrell <mark.andrew.farrell@gmail.com>
2016-11-06 11:22:33 +1100

    Fuses

--- attiny2313/blinky.c
+++ attiny2313/blinky.c
@@ -22,7 +22,34 @@ ISR (TIMER1_COMPA_vect)
 	PINA |= (1 << LED);
 }
 
-void timer1_init()
+void init_timer1_CTC_OCR1A_TOP_ISR(uint16_t TOP)
+{
+// REGISTERS //
+  
+  // OCR1A - Output Compare Register 1 A - Used to set TOP value for Counter 1
+  // TIMSK - Timer Interrupt Mask Register - Used to enable interrupts for counter  
+  // TCCR1A - Timer/Counter Control Register 1 A - Used to configure Timer/Counter 1
+  // TCCR1B - Timer/Counter Control Register 1 B - Used to configure Timer/Counter 1
+
+  // TCCR1A - COM1A0 - Compare Output Mode (Toggle on match)
+  //				 NOTE: Depends on Waveform Generation Mode
+  
+  // TCCR1B - WGM13:0 = 4 (TOP = OCR1A) or 12 (TOP = ICR1) Ref Table 46
+  // TCCR1B - CS12:0 Timer prescaling 
+  //	      NOTE: Must set some bits otherwise the timer has no clock source
+  
+  TCCR1B |= (1 << CS12); //F_CPU/1024
+  
+  TCCR1B |= (1 << WGM12); //Enable CTC mode with OCR1A as TOP
+
+  TCNT1 = 0; //Initialize Counter
+  OCR1A = TOP; 
+  
+  TIMSK |= (1 << OCIE1A); //Enable Interrupt for Channel A of Timer/Counter 1
+  sei(); //Enable Interrupts Globally
+}
+
+void init_timer1_CTC_OCR1A_TOP_OC1A(uint16_t TOP)
 {
 // REGISTERS //
   
@@ -44,19 +71,77 @@ void timer1_init()
   
   TCCR1A |= (1 << COM1A0); //Toggle OC1A on Compare Match.
 
-  TCNT1 = 0;
-  OCR1A = 4100; //Trigger every second
+  TCNT1 = 0; //Initialize Counter
+  OCR1A = TOP;
+}
+
+void init_timer1_CTC_ICR1_TOP_ISR(uint16_t TOP)
+{
+// REGISTERS //
+  
+  // OCR1A - Output Compare Register 1 A - Used to set TOP value for Counter 1
+  // TIMSK - Timer Interrupt Mask Register - Used to enable interrupts for counter  
+  // TCCR1A - Timer/Counter Control Register 1 A - Used to configure Timer/Counter 1
+  // TCCR1B - Timer/Counter Control Register 1 B - Used to configure Timer/Counter 1
+
+  // TCCR1A - COM1A0 - Compare Output Mode (Toggle on match)
+  //				 NOTE: Depends on Waveform Generation Mode
+  
+  // TCCR1B - WGM13:0 = 4 (TOP = OCR1A) or 12 (TOP = ICR1) Ref Table 46
+  // TCCR1B - CS12:0 Timer prescaling 
+  //	      NOTE: Must set some bits otherwise the timer has no clock source
+  
+  TCCR1B |= (1 << CS12); //F_CPU/1024
+  
+  TCCR1B |= (1 << WGM12) | (1 << WGM13); //Enable CTC mode with ICR1 as TOP
+
+  TCNT1 = 0; //Initialize Counter
+  ICR1 = TOP; 
   
   TIMSK |= (1 << OCIE1A); //Enable Interrupt for Channel A of Timer/Counter 1
   sei(); //Enable Interrupts Globally
+}
+
+void init_timer1_CTC_ICR1_TOP_OC1A(uint16_t TOP)
+{
+// REGISTERS //
+  
+  // OCR1A - Output Compare Register 1 A - Used to set TOP value for Counter 1
+  // TIMSK - Timer Interrupt Mask Register - Used to enable interrupts for counter  
+  // TCCR1A - Timer/Counter Control Register 1 A - Used to configure Timer/Counter 1
+  // TCCR1B - Timer/Counter Control Register 1 B - Used to configure Timer/Counter 1
+
+  // TCCR1A - COM1A0 - Compare Output Mode (Toggle on match)
+  //				 NOTE: Depends on Waveform Generation Mode
+  
+  // TCCR1B - WGM13:0 = 4 (TOP = OCR1A) or 12 (TOP = ICR1) Ref Table 46
+  // TCCR1B - CS12:0 Timer prescaling 
+  //	      NOTE: Must set some bits otherwise the timer has no clock source
+  
+  TCCR1B |= (1 << CS12); //F_CPU/1024
+  
+  TCCR1B |= (1 << WGM12) | (1 << WGM13); //Enable CTC mode with ICR1 as TOP
+  
+  TCCR1A |= (1 << COM1A0); //Toggle OC1A on Compare Match.
+
+  TCNT1 = 0; //Initialize Counter
+  ICR1 = TOP;
 }
 
 int main(void) {
   // initialize the direction of PORTD #6 to be an output
   set_output(DDRA, LED);
   set_output(DDRB, PB3);
-
-  timer1_init();
+  
+  uint16_t TOP = 4100; // 0.5 Hz Frequency
+  
+  //init_timer1_CTC_OCR1A_TOP_ISR(TOP);
+  
+  //init_timer1_CTC_OCR1A_TOP_OC1A(TOP);
+  
+  init_timer1_CTC_ICR1_TOP_ISR(TOP);
+  
+  //init_timer1_CTC_ICR1_TOP_OC1A(TOP);
   
   while (1) 
   {
